fc83700bea32d8dd0fa270bb1ee32aeb2c67848f
Jeonjunhowi <jjh63360@kau.kr>
2022-09-24 13:04:02 +0900

    fix: code style

--- models/DUMMYmodel.py
+++ models/DUMMYmodel.py
@@ -3,7 +3,7 @@ class DUMMY_model:
     def __init__(self):
         print("===========================")
         print("어쩌구 저쩌구 설정들")
-    def train(self, data_loader):
+    def forward(self, data_loader):
 
         for i in range (0, len(data_loader)):
             print("conv 넣었다 늘리고 줄이고 우당탕탕... ")
